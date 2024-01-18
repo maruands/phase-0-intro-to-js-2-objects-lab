699d7cf39ec527ce92d9e49a9a90cedc730d62a2
maruands <maruands@gmail.com>
2024-01-18 11:30:47 +0300

    objects in array

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway", 
+} ;
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const newArray = {...employee} ;
+    newArray[key] = value;
+    return newArray
+}
+
+const worker  = updateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+    employee.streetAddress="12 Broadway";
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee,key, value){
+     const newArray = {...employee};
+     delete newArray[key]; 
+     return newArray;
+ }
+const newEmployee  = deleteFromEmployeeByKey(employee, "name", "");
+delete newEmployee.name.valueOf();
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+
+}
